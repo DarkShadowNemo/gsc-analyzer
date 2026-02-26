8c00e5bdc00e1b9b30b2b92be2b65406dabf37ae
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2026-02-26 04:16:44 -0800

    make possible to remove items

--- gsc_analyzer/gsc_analyzer.py
+++ gsc_analyzer/gsc_analyzer.py
@@ -6,108 +6,54 @@ from tkinter import filedialog
 
 window = Tk()
 window.title("GameScene Analyzer")
-window.geometry("256x256")
+window.geometry("256x512")
 
 tree = ttk.Treeview(window)
 tree.pack()
 
-objects=[]
-pads=[]
-vertices=[]
-faces=[]
+def remove_item():
+    selected_item = tree.selection()
+    for item in selected_item:
+        tree.delete(item)
 
 def gamescene_dialog():
-    global filepath
-    filepath = filedialog.askopenfilename()
-    with open(filepath, "rb") as f:
-        while 1:
-            Chunk = f.read(4)
-            if Chunk == b"NU20":
-                FileSize = unpack("<i", f.read(4))[0]
-                PrimType = unpack("<I", f.read(4))[0]
-                unknown = unpack("<I", f.read(4))[0]
-                nu_id = tree.insert("", 0, text=Chunk)
-                tree.insert(nu_id, 1, text="FileSize %d" % abs(FileSize))
-                tree.insert(nu_id, 2, text="PrimType %d" % PrimType)
-                tree.insert(nu_id, 3, text="Unknown1 %d" % unknown)
-            elif Chunk == b"NTBL":
-                NTBLSize1 = unpack("<I", f.read(4))[0]
-                NTBLSize2 = unpack("<I", f.read(4))[0]
-                nu_id1 = tree.insert("", 1, text=Chunk)
-                tree.insert(nu_id1, 2, text="NamedTableSize1 %d" % NTBLSize1)
-                tree.insert(nu_id1, 3, text="NamedTableSize2 %d" % NTBLSize2)
-            elif Chunk == b"TST0":
-                TextureSize = unpack("<I", f.read(4))[0]
-                TextureCount = unpack("<I", f.read(4))[0]
-                Padding = unpack("<I", f.read(4))[0]
-                nu_id2 = tree.insert("", 2, text=Chunk)
-                tree.insert(nu_id2, 3, text="TextureSetSize %d" % TextureSize)
-                tree.insert(nu_id2, 4, text="Texture Count %d" % TextureCount)
-                tree.insert(nu_id2, 5, text="padding %d" % Padding)
-                texture_data_id1 = tree.insert(nu_id2, 6, text="texture data")
-            elif Chunk == b"MS00":
-                MaterialSize = unpack("<I", f.read(4))[0]
-                MaterialCount = unpack("<I", f.read(4))[0]
-                Padding = unpack("<I", f.read(4))[0]
-                nu_id3 = tree.insert("", 3, text=Chunk)
-                tree.insert(nu_id3, 4, text="Material Size %d" % MaterialSize)
-                tree.insert(nu_id3, 5, text="Material Count %d" % MaterialCount)
-                tree.insert(nu_id3, 6, text="padding %d" % Padding)
-                material_data_id1 = tree.insert(nu_id3, 7, text="material data")
-            elif Chunk == b"OBJ0":
-                ObjectSize = unpack("<I", f.read(4))[0]
-                ObjectCount = unpack("<I", f.read(4))[0]
-                Padding = unpack("<I", f.read(4))[0]
-                nu_id4 = tree.insert("", 4, text=Chunk)
-                tree.insert(nu_id4, 5, text="Object Size %d" % ObjectSize)
-                tree.insert(nu_id4, 6, text="Object Count %d" % ObjectCount)
-                tree.insert(nu_id4, 7, text="padding %d" % Padding)
-                offset_data = tree.insert(nu_id4, 8, text="offset data")
-            elif Chunk == b"\x03\x01\x00\x01":
-                f.seek(-4,1)
-                Chunks = unpack("<I", f.read(4))[0]
-                f.seek(2,1)
-                vertexCount = unpack("B", f.read(1))[0]
-                flag1 = unpack("B", f.read(1))[0]
-                fa=-1
-                fb=0
-                fc=1
-                for i in range(vertexCount):
-                    vx = unpack("<f", f.read(4))[0]
-                    vy = unpack("<f", f.read(4))[0]
-                    vz = unpack("<f", f.read(4))[0]
-                    vw = unpack("<f", f.read(4))[0]
-                    vertices.append([vx,vz,vy,1])  
-                datas = tree.insert(offset_data, 5, text=hex(Chunks))
-                tree.insert(datas, 6, text=vertexCount)
-                triangleStrips_ = tree.insert(datas, 7, text=hex(flag1))
-                for i, v in enumerate(vertices,1):
-                    tree.insert(triangleStrips_, 8, text="vx %f" % v[0])
-                    tree.insert(triangleStrips_, 9, text="vz %f" % v[1])
-                    tree.insert(triangleStrips_, 10,text="vy %f" % v[2])
-                    tree.insert(triangleStrips_, 11,text="vw %f" % 1.0)
-                for i in range(vertexCount-2):
-                    fa+=1
-                    fb+=1
-                    fc+=1
-                    faces.append([fa,fb,fc])
-                if PrimType == 6:
-                    
-                    triangleStrips__ = tree.insert("", 8, text="triangle strips")
-                    for i, fabc in enumerate(faces,1):
-                        tree.insert(triangleStrips__, 9, text="fa %d" % fabc[0])
-                        tree.insert(triangleStrips__, 10,text="fb %d" % fabc[1])
-                        tree.insert(triangleStrips__, 11,text="fc %d" % fabc[2])
+    try:
+        global filepath
+        filepath = filedialog.askopenfilename(
+        title="oldttgames gsc",
+        filetypes=[("gsc", "*.gsc")]
+        )
+        with open(filepath, "rb") as f:
+            f.seek(0)
+            Chunks2 = f.read()
+            f.seek(0)
+            while f.tell() < len(Chunks2):
+                Chunk = f.read(4)
+                if Chunk == b"NU20":
+                    FileSize = unpack("<i", f.read(4))[0]
+                    PrimType = unpack("<I", f.read(4))[0]
+                    unknown = unpack("<I", f.read(4))[0]
+                    nu_id = tree.insert("", 0, text=Chunk)
+                    for child in tree.get_children():
                         
-                    
-                
-                
-                    
-            elif Chunk == b"SST0":
-                break
+                        tree.insert(nu_id, 1, text="FileSize %d" % abs(FileSize))
+                        tree.insert(nu_id, 2, text="PrimType %d" % PrimType)
+                        tree.insert(nu_id, 3, text="Unknown1 %d" % unknown)
+                elif Chunk == b"NTBL":
+                    NamedFileSize = unpack("<i", f.read(4))[0]
+                    Namedtablelength = unpack("<i", f.read(4))[0]
+                    nu_id1 = tree.insert("", 4, text=Chunk)
+                    for child in tree.get_children():
+                        tree.insert(nu_id1, 5, text="NamedFileSize %d" % NamedFileSize)
+                        tree.insert(nu_id1, 6, text="Namedtablelength %d" % Namedtablelength)
+                        tree.insert(nu_id1, 7, text="padding01 %d" % padding01)
+    except:
+        OSError
 
 Button1 = Button(window, text="open nuscene", command=gamescene_dialog)
 Button1.pack()
+remove_button = Button(window, text="Remove Selected", command=remove_item)
+remove_button.pack(pady=10)
 
 
 
